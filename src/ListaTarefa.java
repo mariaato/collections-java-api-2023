71b53e1714432e2fd54b4b2425266c6219adf25c
mariaato <mariaaatonha@gmail.com>
2024-06-13 21:31:35 -0300

    teste aula

---
+++ src/ListaTarefa.java
@@ -0,0 +1,51 @@
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class ListaTarefa{
+       private List<Tarefa> tarefaList;
+
+      public ListaTarefa(){
+        this.tarefaList = new ArrayList<>();
+      } 
+
+      public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+      }
+
+      public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+          }
+            tarefaList.removeAll(tarefasParaRemover);
+        
+    }
+            public int obterNumeroTotalTarefas() {
+                return tarefaList.size();
+            }
+        public void obterDescricoesTarefas(){
+            System.out.println(tarefaList);
+        }
+    
+     public static void main(String[] args) {
+        ListaTarefa  listaTarefa = new ListaTarefa();
+
+        System.out.println(" O numero total de elemetentos na lista é:  " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa( "Tarefa 1");
+        listaTarefa.adicionarTarefa( "Tarefa 1");
+        listaTarefa.adicionarTarefa( "Tarefa 2");
+        System.out.println(" O numero total de elemetentos na lista é:  " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 2");
+        System.out.println(" O numero total de elemetentos na lista é:  " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+     }
+}
+
+
